511ae66b9a0208ccf30d80da99a905fd32fe198e
dfraile <dfraile@tecnogram.net>
2019-07-26 17:01:13 +0200

    Updates

---
+++ precache-manifest.6b0ae78f54af44962e3cbc5ebd291a7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2211618b029812f1b62bf2058e8285c7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3205fe955802d7593597",
+    "url": "/monsters-rolodex/static/css/main.242f08b3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3205fe955802d7593597",
+    "url": "/monsters-rolodex/static/js/main.5fb9f171.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
